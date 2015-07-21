e1043d1e58e84fd904de6b7bc688e2eb33f782e0
websbydrew <Developer@Andrews-MacBook-Pro.local>
2015-07-20 21:49:16 -0400

    set up formspree.io

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://formspree.io/andrew@websbydrew.com",
                 type: "POST",
                 data: {
                     name: name,
